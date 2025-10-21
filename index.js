a75c6cf7c72460a116a0a0d7e93807442ec9e52a
gibranrafii <gibranrafi21@gmail.com>
2025-10-21 15:11:12 +0700

    Membuat const dan app use

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
